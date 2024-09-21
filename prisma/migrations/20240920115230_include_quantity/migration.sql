c39d00a292303975d3cd858790c34fabed2d60f7
Jander da Costa Nery <jander.webmaster@gmail.com>
2024-09-21 13:04:40 -0400

    Fxi: remvoe migrations

--- prisma/migrations/20240920115230_include_quantity/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `quantity` to the `products_cart` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "products_cart" ADD COLUMN     "quantity" INTEGER NOT NULL;
